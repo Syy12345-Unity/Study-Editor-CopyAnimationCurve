6ea5a783c0342d8a3b741b66da969172fe67168d
Syy <shirakawa.yy@gmail.com>
2018-08-07 22:42:28 +0900

    Add Code

---
+++ SampleAnimationCurve.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleAnimationCurve : MonoBehaviour {
+
+    public AnimationCurve animationCurve1;
+    public AnimationCurve animationCurve2;
+}
